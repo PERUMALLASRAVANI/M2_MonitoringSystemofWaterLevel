adf10f141add211cb01420877c4e5467f4087bd4
PERUMALLA SRAVANI <101012637+PERUMALLASRAVANI@users.noreply.github.com>
2022-04-21 21:27:47 +0530

    Update Monitoring_system_of_water_level.c

--- 3_Implementation/code/Monitoring_system_of_water_level.c
+++ 3_Implementation/code/Monitoring_system_of_water_level.c
@@ -3,7 +3,7 @@
 
 void USARTInit(uint16_t ubrr_value)
 {
-    //Set Baud rate
+    //
     UBRR0L = ubrr_value;
     UBRR0H = (ubrr_value>>8)&0X00ff ;
     UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
@@ -14,20 +14,20 @@ void USARTInit(uint16_t ubrr_value)
 
 char USARTReadChar()
 {
-    //Wait until data is available
+    //
     while(!(UCSR0A & (1<<RXC0)))
     {
-        //DO NOTHING
+        //...
     }
     return UDR0;
 }
 
 void USARTWriteChar(char data)
 {
-    //wait until transmitter is ready
+    // Transmitter is getting ready
     while (!(UCSR0A & (1<<UDRE0)))
     {
-        //DO  NOTHING
+        //...
     }
     //TO WRITE THE DATA TO USART BUFFER
     UDR0 = data;
