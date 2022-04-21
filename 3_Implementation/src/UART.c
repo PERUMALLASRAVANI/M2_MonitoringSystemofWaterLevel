334e85edaf9a68231154bf21883adbe307551acd
PERUMALLA SRAVANI <101012637+PERUMALLASRAVANI@users.noreply.github.com>
2022-04-21 21:44:17 +0530

    Create UART.c

---
+++ 3_Implementation/src/UART.c
@@ -0,0 +1,34 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+void USARTInit(uint16_t ubrr_value)
+{
+    //Data vis received
+    UBRR0L = ubrr_value;
+    UBRR0H = (ubrr_value>>8)&0X00ff ;
+    UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+
+    //Transmitter and Receiver are working
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
+}
+
+char USARTReadChar()
+{
+    //Pls wait
+    while(!(UCSR0A & (1<<RXC0)))
+    {
+        //...
+    }
+    return UDR0;
+}
+
+void USARTWriteChar(char *data)
+{
+    //Transmitter is getting ready
+    while (!(UCSR0A & (1<<UDRE0)))
+    {
+        //...
+    }
+    //TO WRITE THE DATA TO USART BUFFER
+    UDR0 = data;
+}
