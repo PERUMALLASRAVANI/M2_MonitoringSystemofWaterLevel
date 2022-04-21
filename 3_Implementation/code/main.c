afe502be8e2cf9ccc7f6ba8657428718d403d4be
PERUMALLA SRAVANI <101012637+PERUMALLASRAVANI@users.noreply.github.com>
2022-04-21 20:53:28 +0530

    Update main.c

--- 3_Implementation/code/main.c
+++ 3_Implementation/code/main.c
@@ -1,39 +1,39 @@
-/*
- */
-
 #include <avr/io.h>
 #include <water_level_indication.h>
-//#include <util/delay.h>
+#include <util/delay.h>
 #include "uart.h"
-/*#include <HCSR04.h>
+#include <HCSR04.h>
 #include <NXTBluetooth.h>
 
 #define ULTRASONIC_TRIG_PIN 12
 #define ULTRASONIC_ECHO_PIN 13
 HCSR04 ultrasonic(ULTRASONIC_TRIG_PIN,ULTRASONIC_ECHO_PIN);
-NXTBluetooth bluetooth(2, 3, 1, 10);*/
+NXTBluetooth bluetooth(2, 3, 1, 10);
 
 void USARTInit(uint16_t );
 char USARTReadChar();
 void USARTWriteChar(char *);
 unsigned char mydata;
 int i,duration=0,distance=0;
-//String message; //Bluetooth message
+//String message;
+//Bluetooth message
 
 void USARTInit(uint16_t ubrr_value)
 {
-    //Set Baud rate
+    
+    //Data is received
     UBRR0L = ubrr_value;
     UBRR0H = (ubrr_value>>8)&0X00ff ;
     UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
 
-    //Enable Transmitter Receiver
+    //Transmitter and Receiver are working
     UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
+    
 }
 
 char USARTReadChar()
 {
-    //Wait until data is available
+    //Please wait 
     while(!(UCSR0A & (1<<RXC0)))
     {
         //DO NOTHING
@@ -43,12 +43,12 @@ char USARTReadChar()
 
 void USARTWriteChar(char *data)
 {
-    //wait until transmitter is ready
+    //Transmitter is getting ready
     while (!(UCSR0A & (1<<UDRE0)))
     {
-        //DO  NOTHING
+        //--
     }
-    //TO WRITE THE DATA TO USART BUFFER
+    // WRITE THE DATA TO USART BUFFER
     UDR0 = *data;
 }
 
@@ -68,10 +68,13 @@ int main(void)
             PORTB |=(1<<PORTB2);
             PORTB |=(1<<PORTB4);
             PORTB &=~(1<<PORTB3);
-            /*for (i = 0; i < 10; i += 1) {                                    //FOR BLUETOOTH MESSAGE TRANSFER
+            /*for (i = 0; i < 10; i += 1)
+            {                                   
+            //FOR BLUETOOTH MESSAGE TRANSFER
                 message = String("Motor Turned On " + String(i + 1) + "!");
-            }*/
-            USARTWriteChar("Water level's low, Motor Turned On");/*If interfaced, Message will be sent to Connected Bluetooth user*/
+            }
+            */USARTWriteChar("Water level's low, Motor Turned On");
+            /*If interfaced, Message will be sent to Connected Bluetooth user*/
             //bluetooth.write(message, i);
             //Loads
         }
@@ -82,8 +85,11 @@ int main(void)
             /*for (i = 0; i < 10; i += 1) {                                    //FOR BLUETOOTH MESSAGE TRANSFER
                 message = String("Motor Turned Off " + String(i + 1) + "!");
             }*/
-            USARTWriteChar("Water level's High, Motor Turned Off");/*If interfaced, Message will be sent to Connected Bluetooth user*/
-            //bluetooth.write(message, i);    /*note that messages can't exede 59 characters including the null terminator*/
+            USARTWriteChar("Water level's High, Motor Turned Off");
+            
+            /*If interfaced, Message will be sent to Connected Bluetooth user*/
+            //bluetooth.write(message, i);  
+            /*note that messages can't exede 59 characters including the null terminator*/
         }
         //bluetooth.update();
     }
